3916468ca9f0c1668ddf0ad9e7fb22c365d42479
Mannogroth <william.hjelm@gmail.com>
2016-11-01 13:20:59 +0100

    Added some code to main
    Initialized audioclip for long and short. Main plays SOS now

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,63 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
 /**
  * Created by william on 2016-11-01.
  */
 
 public class Main {
-    public static void main(String[] args) {
-        
+    static int shortStop = 180;
+    static int longStop = 300;
+
+    static Clip shortclip;
+    static Clip longclip;
+
+    public static void playShort() {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("short.wav").getAbsoluteFile());
+            shortclip = AudioSystem.getClip();
+            shortclip.open(audioInputStream);
+            shortclip.start();
+        } catch(Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
+    }
+
+    public static void playLong() {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("long.wav").getAbsoluteFile());
+            longclip = AudioSystem.getClip();
+            longclip.open(audioInputStream);
+            longclip.start();
+        } catch(Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        playShort();
+        Thread.sleep(shortStop);
+        playShort();
+        Thread.sleep(shortStop);
+        playShort();
+        Thread.sleep(shortStop);
+
+        playLong();
+        Thread.sleep(longStop);
+        playLong();
+        Thread.sleep(longStop);
+        playLong();
+        Thread.sleep(longStop);
+
+        playShort();
+        Thread.sleep(shortStop);
+        playShort();
+        Thread.sleep(shortStop);
+        playShort();
+        Thread.sleep(shortStop);
     }
 }
